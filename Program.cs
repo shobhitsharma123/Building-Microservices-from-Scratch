0f53b1b92313142267e856666f2412ca37bf12a5
shobhitsharma123 <63493884+shobhitsharma123@users.noreply.github.com>
2020-05-28 21:42:26 +0530

    34th commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Building_Microservices_from_Scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
